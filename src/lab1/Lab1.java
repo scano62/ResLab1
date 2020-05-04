d5b7df0c6a2e0fc7beb5c8a0276a0b350766a0f2
Requena <ejcrfrz@gmail.com>
2020-05-04 13:33:36 -0500

    inicio

---
+++ src/lab1/Lab1.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author EDUARDO
+ */
+public class Lab1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        while (true) {
+
+            System.out.println("1.Primo");
+            System.out.println("2.Dividir");
+            System.out.println("3.Restar");
+            System.out.println("4.Salir");
+
+            System.out.println("Ingrese opcion");
+            Scanner sc = new Scanner(System.in);
+            String opc = sc.nextLine();
+
+            switch (opc) {
+
+                case "1":
+                   // boolean valor = PRIMO();
+                    //System.out.println("Resultado es : " + valor);
+                case "2":
+
+                case "3":
+
+                case "4":
+
+                    break;
+
+            }
+
+        }
+
+    }
+
+    
+
+}
